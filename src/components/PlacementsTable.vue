204fa889e7733e5e45594c15aad3bdd6bbe2907e
Ryzen <jakubsobota535@gmail.com>
2023-03-16 03:17:13 +0100

    v1.2

--- src/components/PlacementsTable.vue
+++ src/components/PlacementsTable.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
     <h1>Sportsman Placements</h1>
-    <button @click="addNew">Add New</button>
-    <table id="sportsmanTable">
+
+    <button class="btn btn-success btn-sm" @click="addNew">Pridať</button>
+    <table id="sportsmanTable" class="table table-striped table-bordered dt-responsive nowrap no-footer dtr-inline collapsed">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Placement</th>
-          <th>Edit</th>
-          <th>Remove</th>
+          <th>Umiestnenie</th>
+          <th>Hry</th>
+          <th>Disciplína</th>
+          <th>Upraviť</th>
+          <th>Odstrániť</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="sportsman in sportsmen">
-          <td>{{ sportsman.name }}</td>
-          <td>{{ sportsman.placement }}</td>
-          <td><button @click="edit(sportsman)">Edit</button></td>
-          <td><button @click="remove(sportsman)">Remove</button></td>
+        <tr v-for="placement in placements">
+          <td>{{ placement.placement }}</td>
+          <td>{{ placement.games }}</td>
+          <td>{{ placement.discipline }}</td>
+          <td>
+            <button class="btn btn-primary btn-sm" @click="editRow(placement.id)">
+              Upraviť
+            </button>
+          </td>
+          <td>
+            <button class="btn btn-danger btn-sm" @click="removeRow(placement.id)">
+              Odstrániť
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -29,46 +40,53 @@ export default {
     person_id: {
       type: Number,
       default: null,
-    }
+    },
   },
   data() {
     return {
-      placements: [{
-        placement: 0,
-        games: '',          
-        discipline: '',
-      }],
+      table: null,
+      placements: [],
     };
   },
-  created() {    
+  created() {
     $.ajax({
-          type: "GET",
-          contentType: "application/json",
-          url: `/getPlacements.php?person_id=${this.person_id}`
-        }).done(placement => {
-          this.placements.placement = placement.placement;
-          this.placements.games = placement.games;
-          this.placements.discipline = placement.discipline;
+      type: "GET",
+      contentType: "application/json",
+      url: `/api/getPlacements.php?person_id=${this.person_id}`,
+    }).done((placements) => {
+      placements.forEach((placement) => {
+        this.placements.push({
+          id: placement.id,
+          placement: placement.placement,
+          games: placement.games,
+          discipline: placement.discipline,
         });
-
-    $(() => {
-      $("#sportsmanTable").DataTable();
+      });
+      console.log(placements);
+    });
+   
+  },
+  mounted(){ 
+    this.table = $("#sportsmanTable").DataTable({
+      autoWidth: true
     });
   },
   methods: {
     addNew() {
-     
+      console.log("add");
     },
-    edit(sportsman) {},
-    remove(sportsman) {
-      // Remove the sportsman from the database using PHP with MySQLi
-      // Remove the sportsman from the sportsmen array using
-      // this.sportsmen.splice(index, 1)
+    editRow(id) {
+      console.log(id);
+      // this.$router.push({ name: "edit-person", params: id });
+    },
+    deleteRow(id) {
+      console.log("delete");
+      this.table.row(`[data-id="${id}"]`).remove().draw();
+      // $.ajax({
+      //   url: `/deletePerson.php?id=${id}`,
+      //   type: "GET",
+      // });
     },
   },
 };
 </script>
-
-<style>
-/* Your custom styles */
-</style>
